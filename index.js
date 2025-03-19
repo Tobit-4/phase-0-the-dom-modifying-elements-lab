d3789bf2d27ea4cb7222088a47b1b814b0eccfc9
Fidel <fidel.kipkoech@student.moringaschool.com>
2025-03-19 13:18:20 +0300

    made changes

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const removeId = document.getElementById("main");
+
+removeId.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id="victory"
+newHeader.textContent="HI";
+newHeader.textContent="Fidel is the champion";
+
+document.body.appendChild(newHeader);
+
+newHeader.textContent="Fidel is the champion";
